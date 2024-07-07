bd4a145f16d8e6753a589dfa535de8c16d12a972
Hamzat mujeeb <hamzatmujeeb586@gmail.com>
2024-07-07 22:49:45 +0100

    first commit

---
+++ login.js
@@ -0,0 +1,26 @@
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+const images = [
+    'images/Fotolia_239116868_XL.jpg',
+    'images/TomHegen.jpg',
+    'images/Fotolia_251340105_XL.jpg',
+    'images/Fotolia_239116868_XL.jpg' // Add as many as you like
+];
+
+let currentImageIndex = 0;
+
+function changeBackgroundImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    document.querySelector('.body-container').style.backgroundImage = `url(${images[currentImageIndex]})`;
+}
+
+setInterval(changeBackgroundImage, 5000); // Change image every 5 seconds
---
+++ main.js
@@ -0,0 +1,34 @@
+// JavaScript in main.js file or within <script> tag at the end of HTML body
+/*function handleResize() {
+    const behanceHeader = document.querySelector('.behance-header');
+    const formContainer = document.querySelector('.form-container');
+
+    if (window.innerWidth <= 480) {
+        if (!formContainer.contains(behanceHeader)) {
+            formContainer.insertBefore(behanceHeader, formContainer.firstChild);
+        }
+    } else {
+        if (document.body.contains(behanceHeader) && formContainer.contains(behanceHeader)) {
+            document.body.insertBefore(behanceHeader, document.body.firstChild);
+        }
+    }
+}
+
+window.addEventListener('resize', handleResize);
+handleResize(); // Initial call to handleResize on page load*/
+
+const images = [
+    'images/Fotolia_239116868_XL.jpg',
+    'images/TomHegen.jpg',
+    'images/Fotolia_251340105_XL.jpg',
+    'images/Fotolia_239116868_XL.jpg' // Add as many as you like
+];
+
+let currentImageIndex = 0;
+
+function changeBackgroundImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    document.querySelector('.body-container').style.backgroundImage = `url(${images[currentImageIndex]})`;
+}
+
+setInterval(changeBackgroundImage, 5000); // Change image every 5 seconds
